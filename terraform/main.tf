b75276752f75e1aa356a1e21545fefc6e317a6b1
Gvrkprasad <glpsk369@gmail.com>
2025-04-21 15:44:44 +0530

    main.tf updted RT,NAT,EIP

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,3 +1,80 @@
+resource "aws_internet_gateway" "igw" {
+ 
+  tags = {
+    Name = "glps_eks_igw"
+  }
+}
+
+resource "aws_internet_gateway_attachment" "igw_attac" {
+   vpc_id = aws_vpc.main.id
+   internet_gateway_id = aws_internet_gateway.igw.id
+   lifecycle {
+    prevent_destroy = true
+  }
+}
+
+resource "aws_route_table" "rt_custom" {
+  vpc_id = aws_vpc.main.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.igw.id
+  }
+  tags = {
+    Name = "glps_eks_rt_public"
+  }
+}
+
+resource "aws_route_table_association" "custom" {
+    count = 2
+    subnet_id = aws_subnet.public_subnets[count.index].id
+    route_table_id = aws_route_table.rt_custom.id
+}
+
+resource "aws_route_table" "rt_main1" {
+    vpc_id = aws_vpc.main.id
+
+    route {
+      cidr_block = "0.0.0.0/0"
+      gateway_id = aws_nat_gateway.nat.id
+    }
+    tags ={
+      Name = "glps_eks_rt_private"
+    }
+}
+
+resource "aws_route_table_association" "main1" {
+  count = 2
+  subnet_id = aws_subnet.private_subnets[count.index].id
+  route_table_id = aws_route_table.rt_main1.id
+}
+
+resource "aws_route_table" "rt_main2" {
+  vpc_id = aws_vpc.main.id
+
+  tags ={
+    Name = "glps_eks_rt_db"
+  }
+
+}
+
+resource "aws_eip" "eip" {
+  domain = "vpc"
+
+  tags = {
+    Name = "glps_eks_eip"
+  }
+}
+
+resource "aws_nat_gateway" "nat" {
+  subnet_id = aws_subnet.public_subnets[0].id
+  allocation_id = aws_eip.eip.id  
+
+  tags = {
+    Name = "glps_eks_nat"
+  }
+}
+
 resource "aws_vpc" "main" {
   cidr_block = var.vpc_cidr
   enable_dns_hostnames = true

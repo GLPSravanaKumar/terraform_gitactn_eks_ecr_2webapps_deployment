f9d18aae4385b295793c3200d7f6e435f0adaa6b
Gvrkprasad <glpsk369@gmail.com>
2025-04-26 23:07:04 +0530

    sg & helm_release,Iam updated in main.tf

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -62,6 +62,17 @@ provider "helm" {
   kubernetes {
     host                   = aws_eks_cluster.eks.endpoint
     cluster_ca_certificate = base64decode(aws_eks_cluster.eks.certificate_authority[0].data)
-    token                  = data.aws_eks_cluster_auth.eks.token
-  } 
+    exec {
+      api_version = "client.authentication.k8s.io/v1beta1"
+      command     = "aws"
+      args = [
+        "eks",
+        "get-token",
+        "--cluster-name",
+        aws_eks_cluster.eks.name,
+        "--region",
+        var.region
+      ]
+    }
+  }
 }

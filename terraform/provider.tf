7ac87d65d67f7d0fc06c3d6ace47ae9cfe72a4f1
Gvrkprasad <glpsk369@gmail.com>
2025-04-26 13:41:20 +0530

    provider.tf updated data 'aws_eks_cluster' source

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -20,21 +20,28 @@ provider "aws" {
   region = var.region
 }
 
+data "aws_eks_cluster" "eks" {
+  name = var.cluster_name
+  depends_on = [ aws_eks_cluster.eks ]
+}
+
 data "aws_eks_cluster_auth" "eks" {
   name = var.cluster_name
   depends_on = [ aws_eks_cluster.eks ]
 }
 
+
+
 provider "kubernetes" {
-  host                   = aws_eks_cluster.eks.endpoint
-  cluster_ca_certificate = base64decode(aws_eks_cluster.eks.certificate_authority[0].data)
+  host                   = data.aws_eks_cluster.eks.endpoint
+  cluster_ca_certificate = base64decode(data.aws_eks_cluster.eks.certificate_authority[0].data)
   token                  = data.aws_eks_cluster_auth.eks.token
  }
 
 provider "helm" {
   kubernetes {
-    host                   = aws_eks_cluster.eks.endpoint
-    cluster_ca_certificate = base64decode(aws_eks_cluster.eks.certificate_authority[0].data)
+    host                   = data.aws_eks_cluster.eks.endpoint
+    cluster_ca_certificate = base64decode(data.aws_eks_cluster.eks.certificate_authority[0].data)
     token                  = data.aws_eks_cluster_auth.eks.token
   }
 }

cd2336b8812f63af6f50f79071c3d2396f1b6e75
Gvrkprasad <glpsk369@gmail.com>
2025-05-08 11:48:02 +0530

    lb DNS var added in output.tf

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -7,4 +7,7 @@ output "cluster_endpoint" {
   value = aws_eks_cluster.eks.endpoint
 }
 
-
+output "service_loadbalancer_dns" {
+  value = kubernetes_service.webapp.status[0].load_balancer[0].ingress[0].hostname
+  description = "The DNS name of the LoadBalancer"
+}

db6e7e7bc3e494f1987fdbdeee5b614e2f01505e
Gvrkprasad <glpsk369@gmail.com>
2025-05-08 14:46:34 +0530

    output lb_dns added

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -7,7 +7,7 @@ output "cluster_endpoint" {
   value = aws_eks_cluster.eks.endpoint
 }
 
-/*
+
 output "service_loadbalancer_dns" {
   value = kubernetes_ingress_v1.webapp_ingress.status[0].load_balancer[0].ingress[0].hostname
   description = "The DNS name of the LoadBalancer"
@@ -16,4 +16,4 @@ output "service_loadbalancer_dns" {
     kubernetes_ingress_v1.webapp_ingress
   ]
 }
-*/
+
